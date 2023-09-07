627f4489a314b70b245d81ac550154864b3fb502
HlisTilen <tilen.hlis@gmail.com>
2023-09-07 11:42:53 +0200

    fix: functional

--- kotlin-functional/src/main/kotlin/extension/ZagonskiExtension.kt
+++ kotlin-functional/src/main/kotlin/extension/ZagonskiExtension.kt
@@ -6,11 +6,43 @@ fun main() {
         UdelezenecKonference("Ana", "Krajnc", 1)
     )
 
-    // Filtriranje visoko sodelujočih udeležencev z uporabo extension funkcije
-    //val visokoSodelujoci = udelezenciKonference.filtrirajExtension { it.steviloSodelovanj >= 3 }
-    //println("Visoko sodelujoči udeleženci: $visokoSodelujoci")
+    // Izračun povprečnega števila sodelovanj z uporabo extension funkcije
+    val povprecnaSodelovanja = udelezenciKonference.izracunajPovprecnoSodelovanjeExtension()
+    println("Povprečno število sodelovanj: $povprecnaSodelovanja")
 
+    // TODO Filtriranje visoko sodelujočih udeležencev z uporabo extension funkcije
+
+    // Preslikava seznama udeležencev v seznam imen z uporabo extension funkcije
+    val imenaUdelezencev = udelezenciKonference.preslikajExtension { "${it.ime} ${it.priimek}" }
+    println("Seznam imen udeležencev: $imenaUdelezencev")
+
+    // Sprememba števila sodelovanj udeleženca z uporabo extension funkcije
+    val noviUdelezenciKonference = udelezenciKonference.spremeniSteviloSodelovanjExtension("Janez Novak", 6)
+    println("Seznam udeležencev po spremembi števila sodelovanj: $noviUdelezenciKonference")
+}
+
+// Extension funkcija za izračun povprečnega števila sodelovanj
+fun List<UdelezenecKonference>.izracunajPovprecnoSodelovanjeExtension(): Double {
+    return this.sumOf { it.steviloSodelovanj }.toDouble() / this.size
 }
 
 // TODO Extension funkcija za filtriranje seznama udeležencev
 
+// Extension funkcija za preslikavo seznama udeležencev v seznam imen
+fun List<UdelezenecKonference>.preslikajExtension(preslikava: (UdelezenecKonference) -> String): List<String> {
+    return this.map(preslikava)
+}
+
+// Extension funkcija za spreminjanje števila sodelovanj udeleženca
+fun List<UdelezenecKonference>.spremeniSteviloSodelovanjExtension(
+    imePriimek: String,
+    novoStevilo: Int
+): List<UdelezenecKonference> {
+    return this.map { udelezenec ->
+        if ("${udelezenec.ime} ${udelezenec.priimek}" == imePriimek) {
+            udelezenec.copy(steviloSodelovanj = novoStevilo)
+        } else {
+            udelezenec
+        }
+    }
+}
--- kotlin-functional/src/main/kotlin/nonFunctional/ZagonskiNon.kt
+++ kotlin-functional/src/main/kotlin/nonFunctional/ZagonskiNon.kt
@@ -6,15 +6,36 @@ fun main() {
         UdelezenecKonferenceNon("Ana", "Krajnc", 4)
     )
 
-    // Filtriranje udeležencev z visokim številom preteklih udeležb z zanko
-    val visokoUdelezeni = mutableListOf<UdelezenecKonferenceNon>()
+    // Izračun povprečnega števila preteklih udeležb z zanko
+    var skupnoUdelezb = 0
     for (udelezenec in udelezenciKonference) {
-        if (udelezenec.preteklaUdelezba >= 3) {
-            visokoUdelezeni.add(udelezenec)
-        }
+        skupnoUdelezb += udelezenec.preteklaUdelezba
     }
-    println("Udeleženci z visokim številom preteklih udeležb: $visokoUdelezeni")
+    val povprecnaUdelezba = skupnoUdelezb.toDouble() / udelezenciKonference.size
+    println("Povprečno število preteklih udeležb: $povprecnaUdelezba")
 
+    // TODO Filtriranje udeležencev z visokim številom preteklih udeležb z zanko
+
+    //println("Udeleženci z visokim številom preteklih udeležb: $visokoUdelezeni") TODO uncomment
+
+    // Preslikava seznama udeležencev v seznam imen z zanko
+    val imenaUdelezencev = mutableListOf<String>()
+    for (udelezenec in udelezenciKonference) {
+        imenaUdelezencev.add("${udelezenec.ime} ${udelezenec.priimek}")
+    }
+    println("Imena udeležencev: $imenaUdelezencev")
+
+    // Sprememba števila preteklih udeležb za udeleženca z uporabo nečiste funkcije
+    spremeniUdelezbo(udelezenciKonference, "Janez Novak", 4)
+    println("Udeleženci konference po spremembi udeležbe: $udelezenciKonference")
 }
 
-
+// Nečista funkcija za spremembo števila preteklih udeležb za udeleženca
+fun spremeniUdelezbo(udelezenci: MutableList<UdelezenecKonferenceNon>, polnoIme: String, novaUdelezba: Int) {
+    for (udelezenec in udelezenci) {
+        if ("${udelezenec.ime} ${udelezenec.priimek}" == polnoIme) {
+            udelezenec.preteklaUdelezba = novaUdelezba
+            return // Izhod iz zanke po najdenem udeležencu
+        }
+    }
+}
